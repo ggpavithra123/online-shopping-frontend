4ab120663f1404849e759d732baf6d3ccea2e3d2
ggpavithra123 <ggpavi12@gmail.com>
2026-02-24 16:04:21 +0530

    position command

--- src/components/product/ProductDetail.js
+++ src/components/product/ProductDetail.js
@@ -124,7 +124,7 @@ export default function ProductDetail() {
                         dispatch(addCartItem(product._id, quantity))
                         toast('Cart Item Added!',{
                             type: 'success',
-                            position: toast.POSITION.BOTTOM_CENTER
+                            position: "bottom-center"
                         })
                     }}
                      className="btn btn-primary d-inline ml-4"
