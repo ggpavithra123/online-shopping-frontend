12ee64d50512526fdddc3773b609f6f4c73f9195
ggpavithra123 <ggpavi12@gmail.com>
2026-02-26 11:36:27 +0530

    cartItems removal OrderSuccess page

--- src/components/cart/OrderSuccess.js
+++ src/components/cart/OrderSuccess.js
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { orderCompleted } from "../../slices/cartSlice";
+import { Link } from "react-router-dom";
+
 export default function OrderSuccess() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(orderCompleted());
+    }, [dispatch]);
+
     return (
         <div className="row justify-content-center">
             <div className="col-6 mt-5 text-center">
-                <img className="my-5 img-fluid d-block mx-auto" src="/images/success.png" alt="Order Success" width="200" height="200" />
+                <img
+                    className="my-5 img-fluid d-block mx-auto"
+                    src="/images/success.png"
+                    alt="Order Success"
+                    width="200"
+                    height="200"
+                />
 
                 <h2>Your Order has been placed successfully.</h2>
 
-                <a href="/orders">Go to Orders</a>
+                <Link to="/orders">Go to Orders</Link>
             </div>
-
         </div>
-    )
+    );
 }
