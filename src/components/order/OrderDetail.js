cdff120170ef4ef58f1096af833c685f93ecf617
ggpavithra123 <ggpavi12@gmail.com>
2026-02-26 15:40:23 +0530

    adding more design elements to the page

--- src/components/order/OrderDetail.js
+++ src/components/order/OrderDetail.js
@@ -1,77 +1,165 @@
 import { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import  Loader from '../Layouts/Loader';
-import {orderDetail as orderDetailAction } from '../../actions/orderActions';
-export default function OrderDetail () {
-    const { orderDetail, loading } = useSelector(state => state.orderState)
-    const { shippingInfo={}, user={}, orderStatus="Processing", orderItems=[], totalPrice=0, paymentInfo={} } = orderDetail;
-    const isPaid = paymentInfo && paymentInfo.status === "succeeded" ? true: false;
+import Loader from '../Layouts/Loader';
+import { orderDetail as orderDetailAction } from '../../actions/orderActions';
+
+export default function OrderDetail() {
+    const { orderDetail, loading } = useSelector(state => state.orderState);
+
+    const {
+        shippingInfo = {},
+        user = {},
+        orderStatus = "Processing",
+        orderItems = [],
+        totalPrice = 0,
+        paymentInfo = {}
+    } = orderDetail;
+
+    const isPaid = paymentInfo && paymentInfo.status === "succeeded";
     const dispatch = useDispatch();
-    const {id } = useParams();
+    const { id } = useParams();
 
     useEffect(() => {
-    if(id){
-        dispatch(orderDetailAction(id));
-    }
-}, [dispatch, id]);
+        if (id) {
+            dispatch(orderDetailAction(id));
+        }
+    }, [dispatch, id]);
 
     return (
         <Fragment>
-            {   loading ? <Loader/> :
-                <Fragment>
-                    <div className="row d-flex justify-content-between">
-                        <div className="col-12 col-lg-8 mt-5 order-details">
-    
-                            <h1 className="my-5">Order # {orderDetail._id}</h1>
-    
-                            <h4 className="mb-4">Shipping Info</h4>
-                            <p><b>Name:</b> {user.name}</p>
-                            <p><b>Phone:</b> {shippingInfo.phoneNo}</p>
-                            <p className="mb-4"><b>Address:</b>{shippingInfo.address}, {shippingInfo.city}, {shippingInfo.postalCode}, {shippingInfo.state}, {shippingInfo.country}</p>
-                            <p><b>Amount:</b> ${totalPrice}</p>
-    
-                            <hr />
-    
-                            <h4 className="my-4">Payment</h4>
-                            <p className={isPaid ? 'greenColor' : 'redColor' } ><b>{isPaid ? 'PAID' : 'NOT PAID' }</b></p>
-    
-    
-                            <h4 className="my-4">Order Status:</h4>
-                            <p className={orderStatus&&orderStatus.includes('Delivered') ? 'greenColor' : 'redColor' } ><b>{orderStatus}</b></p>
-    
-    
-                            <h4 className="my-4">Order Items:</h4>
-    
-                            <hr />
-                            <div className="cart-item my-1">
-                                {orderItems && orderItems.map(item => (
-                                    <div className="row my-5">
-                                        <div className="col-4 col-lg-2">
-                                            <img src={item.image} alt={item.name} height="45" width="65" />
-                                        </div>
+            {loading ? <Loader /> :
+                <div className="container my-5">
+                    <div className="row">
 
-                                        <div className="col-5 col-lg-5">
-                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
-                                        </div>
+                        {/* LEFT SIDE */}
+                        <div className="col-lg-8">
 
-
-                                        <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                                            <p>${item.price}</p>
-                                        </div>
-
-                                        <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                                            <p>{item.quantity} Piece(s)</p>
-                                        </div>
-                                    </div>
-                                ))}
-                                    
+                            {/* Order Header */}
+                            <div className="card shadow-sm mb-4 border-0">
+                                <div className="card-body">
+                                    <h2 className="mb-3">
+                                        Order Details
+                                    </h2>
+                                    <h5 className="text-muted">
+                                        Order ID: <span className="text-dark">{orderDetail._id}</span>
+                                    </h5>
+                                </div>
                             </div>
-                            <hr />
+
+                            {/* Shipping Info */}
+                            <div className="card shadow-sm mb-4 border-0">
+                                <div className="card-body">
+                                    <h4 className="mb-3 text-primary">
+                                        <i className="fa fa-truck me-2"></i> Shipping Information
+                                    </h4>
+                                    <p><strong>Name:</strong> {user.name}</p>
+                                    <p><strong>Phone:</strong> {shippingInfo.phoneNo}</p>
+                                    <p>
+                                        <strong>Address:</strong><br />
+                                        {shippingInfo.address}, {shippingInfo.city},<br />
+                                        {shippingInfo.postalCode}, {shippingInfo.state}, {shippingInfo.country}
+                                    </p>
+                                </div>
+                            </div>
+
+                            {/* Payment Info */}
+                            <div className="card shadow-sm mb-4 border-0">
+                                <div className="card-body">
+                                    <h4 className="mb-3 text-success">
+                                        <i className="fa fa-credit-card me-2"></i> Payment Status
+                                    </h4>
+                                    <span className={`badge ${isPaid ? "bg-success" : "bg-danger"} p-2`}>
+                                        {isPaid ? "PAID" : "NOT PAID"}
+                                    </span>
+                                </div>
+                            </div>
+
+                            {/* Order Status */}
+                            <div className="card shadow-sm mb-4 border-0">
+                                <div className="card-body">
+                                    <h4 className="mb-3 text-warning">
+                                        <i className="fa fa-info-circle me-2"></i> Order Status
+                                    </h4>
+                                    <span className={`badge ${orderStatus.includes("Delivered") ? "bg-success" : "bg-warning text-dark"} p-2`}>
+                                        {orderStatus}
+                                    </span>
+                                </div>
+                            </div>
+
+                            {/* Order Items */}
+                            <div className="card shadow-sm border-0">
+                                <div className="card-body">
+                                    <h4 className="mb-4 text-dark">
+                                        <i className="fa fa-shopping-cart me-2"></i> Ordered Items
+                                    </h4>
+
+                                    {orderItems.map((item, index) => (
+                                        <div key={index} className="row align-items-center mb-4 border-bottom pb-3">
+                                            <div className="col-3 col-md-2">
+                                                <img
+                                                    src={item.image}
+                                                    alt={item.name}
+                                                    className="img-fluid rounded"
+                                                />
+                                            </div>
+
+                                            <div className="col-9 col-md-4">
+                                                <Link
+                                                    to={`/product/${item.product}`}
+                                                    className="text-decoration-none fw-bold text-dark"
+                                                >
+                                                    {item.name}
+                                                </Link>
+                                            </div>
+
+                                            <div className="col-6 col-md-3 mt-2 mt-md-0">
+                                                <span className="text-muted">
+                                                    ${item.price}
+                                                </span>
+                                            </div>
+
+                                            <div className="col-6 col-md-3 mt-2 mt-md-0">
+                                                <span className="badge bg-secondary">
+                                                    Qty: {item.quantity}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
+
+                        {/* RIGHT SIDE - ORDER SUMMARY */}
+                        <div className="col-lg-4">
+                            <div className="card shadow-lg border-0 sticky-top" style={{ top: "100px" }}>
+                                <div className="card-body">
+                                    <h4 className="mb-4 text-center">Order Summary</h4>
+
+                                    <div className="d-flex justify-content-between mb-2">
+                                        <span>Items:</span>
+                                        <span>{orderItems.length}</span>
+                                    </div>
+
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <span>Total Price:</span>
+                                        <strong>${totalPrice}</strong>
+                                    </div>
+
+                                    <hr />
+
+                                    <div className="text-center">
+                                        <span className="badge bg-dark p-2">
+                                            Thank you for shopping with us!
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-                </Fragment>
+                </div>
             }
         </Fragment>
-    )
+    );
 }
